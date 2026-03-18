6e9ce34ed15e9bb498f2242c2d6a420a9b4cda03
chris2007-t <chrismagnoungou@gmail.com>
2026-03-18 18:01:49 +0100

    ajout des commentaires dans les fichiers report.sh et cleanup.sh

--- syskit/modules/cleanup.sh
+++ syskit/modules/cleanup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Récupère le chemin passé en argument
 chemin="$1"
+# Initialise un compteur de fichiers supprimés
 count=0
 
+# Vérifie si aucun argument n’a été donné
 if test "$chemin" = ""
 then
 echo "Erreur : chemin manquant"
 exit 1
 fi
+# Vérifie si le chemin fourni est bien un dossier existant
 
 if test ! -d "$chemin" 
 then
@@ -15,20 +19,29 @@ echo "Erreur : dossier invalide"
 exit 1
 fi
 
+# Parcourt tous les fichiers du dossier
+for fichier in "$chemin"/*
 do
 
+ # Vérifie si le fichier a une extension .tmp, .log ou .tar	
 if [[ "$fichier" == *.tmp || "$fichier" == *.log || "$fichier" == *.tar ]]
 then
 
+# Supprime le fichier	
 rm "$fichier"
+
+# Affiche le nom du fichier supprimé
 echo "Supprimé : $fichier"
 
+# Incrémente le compteur
 count=$((count+1))
 
 fi
 
 done
 
+# Affiche le nombre total de fichiers supprimés
 echo "$count fichiers supprimés"
 
+# Ajoute un log avec la date dans le fichier logs/cleanup.log
 echo "$count fichiers supprimés le $(date)" >> logs/cleanup.log
--- syskit/modules/report.sh
+++ syskit/modules/report.sh
@@ -1,61 +1,79 @@
 #!/bin/bash
 
+# =====================================================
+# MODULE REPORT
+# Ce script génère un rapport système complet
+# =====================================================
+
+# définition du fichier rapport
 
 rapport="reports/report.txt"
 
+# création du rapport (écrase l'ancien)
 
 echo "===== RAPPORT SYSKIT =====" > "$rapport"
 
-
+# ajout de la date
 echo "Date : $(date)" >> "$rapport"
 
 echo "" >> "$rapport"
 
+# =====================================================
+# 1. INFORMATIONS SYSTEME
+# =====================================================
 
 echo "===== INFORMATIONS SYSTEME =====" >> "$rapport"
 
-
+# afficher les infos système
 uname -a >> "$rapport"
 
 echo "" >> "$rapport
+
+# =====================================================
+# 2. SAUVEGARDES REALISEES
+# =====================================================
 
 echo "===== SAUVEGARDES REALISEES =====" >> "$rapport"
 
 # vérifier si le fichier backup.log existe
 if [ -f logs/backup.log ]
 then
-    
+
+# afficher le contenu    	
     cat logs/backup.log >> "$rapport"
 else
 
-
+# si aucune sauvegarde
     echo "Aucune sauvegarde réalisée" >> "$rapport"
 fi
 
 echo "" >> "$rapport"
 
+# =====================================================
+# 3. NOMBRE DE FICHIERS SUPPRIMES
+# =====================================================
 
 echo "===== NETTOYAGE =====" >> "$rapport"
 
-
+# vérifier si le fichier cleanup.log existe
 if [ -f logs/cleanup.log ]
 then
 
-    
+# récupérer le nombre total de fichiers supprimés    
     total=0
 
-    
+# récupérer le nombre total de fichiers supprimés    
     while read ligne
     do
-        
+# extraire le nombre (premier mot de la ligne)        
         nombre=$(echo "$ligne" | awk '{print $1}')
 
-        
+# additionner        
         total=$((total + nombre))
 
     done < logs/cleanup.log
 
-    
+# afficher le total    
     echo "Nombre total de fichiers supprimés : $total" >> "$rapport"
 
 else
