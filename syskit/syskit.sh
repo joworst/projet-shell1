f99acf88fafa351da575a14ca008a7ddc5069943
joworst <jeandelafrance14@gmail.com>
2026-03-18 16:25:26 +0100

    ajouts de commentaires

--- syskit/syskit.sh
+++ syskit/syskit.sh
@@ -1,33 +1,48 @@
 #!/bin/bash
 
+# =====================================================
+# SCRIPT PRINCIPAL : syskit.sh
+# Ce script permet de lancer les différentes fonctionnalités
+# du projet (inventory, backup, cleanup, report)
+# =====================================================
+
+# récupération de la première commande entrée par l'utilisateur
+
 commande="$1"
 
-if [ -z "$commande" ]
+# -----------------------------------------------------
+# Vérification si aucune commande n'a été donnée
+# -----------------------------------------------------
+if test  "$commande" = ""
 then
 echo "Erreur : aucune commande donnée"
 echo "Usage : ./syskit.sh {inventory|backup|cleanup|report}"
-exit 1
-fi
+exit 1  # arrêt du programme
+fi 
+
+# -----------------------------------------------------
+# Structure de décision pour choisir la fonctionnalité
+# -----------------------------------------------------
 
 case "$commande" in
 
-inventory)
+inventory)# appel du module inventory
 bash modules/inventory.sh
 ;;
 
-backup)
+backup)# appel du module backup
 bash modules/backup.sh "$2"
 ;;
 
-cleanup)
+cleanup)# appel du module cleanup
 bash modules/cleanup.sh "$2"
 ;;
 
-report)
+report)# appel du module report
 bash modules/report.sh
 ;;
 
-*)
+*) # cas où la commande n'existe pas
 echo "Commande inconnue"
 ;;
 
