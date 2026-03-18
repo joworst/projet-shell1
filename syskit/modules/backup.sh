67124140ab067e9f9da92b527f2c12c03eb2b126
Andem <andemgraslya7@gmail.com>
2026-03-18 18:00:49 +0100

    ajouts de commentaires des fichiers inventory.sh et backup.sh

--- syskit/modules/backup.sh
+++ syskit/modules/backup.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+# Récupère le premier argument passé au script
+# (le dossier que l'utilisateur veut sauvegarder)
 dossier="$1"
 
+# Vérifie si aucun argument n'a été donné
+# Si la variable dossier est vide, on affiche une erreur
 if test "$dossier" = ""
 then
 echo "Erreur : veuillez donner un dossier"
-exit 1
+exit 1    # exit 1 arrête le script avec un code d'erreur
 fi
 
-if test ! -d "$dossier" 
+
+# Vérifie si le dossier donné existe
+if test ! -d "$dossier"       # -d permet de tester si c'est bien un répertoire
 then
 echo "Erreur : dossier inexistant"
-exit 1
+exit 1   # arrêt du script si le dossier n'existe pas
 fi
+
+# Récupère la date actuelle au format année-mois-jour
 date=$(date +%Y-%m-%d)
 nom=$(basename "$dossier")
 mkdir -p backup
@@ -22,4 +30,5 @@ tar -cvf "$archive" "$dossier"
 
 echo "Sauvegarde créée : $archive"
 
+# Ajoute le nom de l'archive dans un fichier log
 echo "$archive" >> logs/backup.log
--- syskit/modules/inventory.sh
+++ syskit/modules/inventory.sh
@@ -1,36 +1,50 @@
 #!/bin/bash
-
+# Ce script affiche différentes informations sur le système Linux
+# Fonction qui affiche le système d'exploitation utilisé
 show_os() {
 echo "Système d'exploitation :"
 uname -o
-}
+} # uname -o affiche le nom du système d'exploitation
 
+# Fonction qui affiche la version du noyau Linux
 show_kernel() {
 echo "Version du noyau :"
 uname -r
-}
+} # uname -r affiche la version du kernel (noyau)
 
+
+# Fonction qui affiche les informations sur le processeur
 show_cpu() {
 echo "Processeur :"
 lscpu | grep "Model name"
-}
+}  # lscpu affiche les informations du CPU
+    # grep permet de filtrer uniquement la ligne contenant "Model name"
 
+
+ # Fonction qui affiche l'état de la mémoire RAM   
 show_ram() {
-echo "Mémoire RAM :"
+echo "Mémoire RAM :" # free -h affiche l'utilisation de la RAM en format lisible (h = human readable)
 free -h
 }
+
+# Fonction qui affiche l'espace disque disponible
 show_disk() {
 echo "Espace disque :"
 df -h
-}
+}   # df -h affiche l'utilisation des disques avec une taille lisible
 
+
+# Fonction qui affiche l'adresse IP de la machine
 show_ip() {
-echo "Adresse IP :"
+echo "Adresse IP :"   # hostname -I affiche les adresses IP de l'ordinateur
 hostname -I
 }
 
+# Titre affiché avant les informations système
 echo "===== INVENTORY ====="
 
+
+# Appel des fonctions pour afficher toutes les informations
 show_os
 show_kernel
 show_cpu
