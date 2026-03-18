a0b30bf6c288afc96bfaae7192b450e4a0daa49f
Andem <andemgraslya7@gmail.com>
2026-03-18 17:00:21 +0100

    Amelioration de code du fichier inventory

--- syskit/modules/inventory.sh
+++ syskit/modules/inventory.sh
@@ -19,3 +19,21 @@ show_ram() {
 echo "Mémoire RAM :"
 free -h
 }
+show_disk() {
+echo "Espace disque :"
+df -h
+}
+
+show_ip() {
+echo "Adresse IP :"
+hostname -I
+}
+
+echo "===== INVENTORY ====="
+
+show_os
+show_kernel
+show_cpu
+show_ram
+show_disk
+show_ip
