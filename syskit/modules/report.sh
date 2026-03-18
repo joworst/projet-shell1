4492381e448bec4675f5c6f48069da06f481b956
chris2007-t <chrismagnoungou@gmail.com>
2026-03-18 17:41:58 +0100

    amélioratin du code du fichier report.sh

--- syskit/modules/report.sh
+++ syskit/modules/report.sh
@@ -24,6 +24,44 @@ echo "===== SAUVEGARDES REALISEES =====" >> "$rapport"
 # vérifier si le fichier backup.log existe
 if [ -f logs/backup.log ]
 then
-    # afficher le contenu
+    
     cat logs/backup.log >> "$rapport"
 else
+
+
+    echo "Aucune sauvegarde réalisée" >> "$rapport"
+fi
+
+echo "" >> "$rapport"
+
+
+echo "===== NETTOYAGE =====" >> "$rapport"
+
+
+if [ -f logs/cleanup.log ]
+then
+
+    
+    total=0
+
+    
+    while read ligne
+    do
+        
+        nombre=$(echo "$ligne" | awk '{print $1}')
+
+        
+        total=$((total + nombre))
+
+    done < logs/cleanup.log
+
+    
+    echo "Nombre total de fichiers supprimés : $total" >> "$rapport"
+
+else
+    echo "Aucun nettoyage effectué" >> "$rapport"
+fi
+
+# message final
+echo ""
+echo "Rapport généré dans $rapport"
